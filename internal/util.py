0c63a45755eb492192207dcf0561231183634869
Gareth Coles <gdude2002@gmail.com>
2014-08-14 15:35:28 +0100

    Cut down on logging spam from dict2xml :U

--- internal/util.py
+++ internal/util.py
@@ -5,12 +5,14 @@ import sys
 
 logging.basicConfig(
     format="%(asctime)s | %(levelname)8s | %(message)s",
-    datefmt="%d %b %Y - %H:%M:%S",
-    level=(logging.DEBUG if "--debug" in sys.argv else logging.INFO))
+    datefmt="%d %b %Y - %H:%M:%S")
+
+logger = logging.getLogger("Web")
+logger.setLevel(logging.DEBUG if "--debug" in sys.argv else logging.INFO)
 
 
 def log(message, level=logging.INFO):
-    logging.log(level, message)
+    logger.log(level, message)
 
 
 def log_request(request, message, level=logging.INFO):
